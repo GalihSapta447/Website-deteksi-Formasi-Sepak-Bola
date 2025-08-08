6794a4dc00fc61285a8d56b4e0b03d4e321db888
GalihSapta447 <sapta.galih@gmail.com>
2025-08-08 13:42:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -308,7 +308,7 @@ elif page_key == "detect":
         with col2:
             st.image(img, caption="Gambar yang Diunggah", use_container_width=True)
 
-        col1, col2, col3 = st.columns([1, 2, 1])
+        col1, col2, col3 = st.columns([1, 4, 1])
         with col2:
             if st.button("🔎 Deteksi Formasi", key="predict_btn"):
                 with st.spinner("Sedang mengdeteksi gambar..."):
@@ -510,6 +510,7 @@ elif page_key == "history":
             <p style="color: #666;">Lakukan deteksi formasi terlebih dahulu untuk melihat riwayat</p>
         </div>
         """, unsafe_allow_html=True)
+
 
 
 
